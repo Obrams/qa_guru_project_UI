2a9c7f628f6a67ae9da8f4b456d0b3f64195757c
NOzerov <testirochik@gmail.com>
2023-10-06 09:31:20 +0300

    add two test for basket

--- pages/products_page.py
+++ pages/products_page.py
@@ -1,8 +1,29 @@
-from selene import browser, have
+from selene import browser, have, be
 
 
 class Products_page:
     def add_product_for_basket(self):
-        browser.all('[class^=inventory_item][class*=inventory_item_label]').element_by(have.Browser.element('#add-to-cart-sauce-labs-backpack')).click()
+        browser.element('#add-to-cart-sauce-labs-backpack').click()
+        browser.element('.shopping_cart_badge').should(be.visible)
         return self
-    #// *[ @ id = "add-to-cart-sauce-labs-backpack"]
+
+    def open_backet(self):
+        browser.element('.shopping_cart_link').click()
+        return self
+
+    def check_success_add_product(self, name, price):
+        browser.element('.inventory_item_name').should(have.exact_text(name))
+        browser.element('.inventory_item_price').should(have.exact_text(f'${price}'))
+        browser.element('.title').should(have.text('Your Cart'))
+        return self
+
+
+    def remove_product_for_basket(self):
+        browser.element('#remove-sauce-labs-backpack').click()
+        return self
+
+    def check_after_remove_product(self):
+        browser.element('.inventory_item_name').should(be.hidden)
+        browser.element('.inventory_item_desc').should(be.hidden)
+        browser.element('.shopping_cart_badge').should(be.hidden)
+        return self
--- tests/test_products_page.py
+++ tests/test_products_page.py
@@ -1,3 +1,5 @@
+import allure
+
 from pages.main_page import MainPage
 from pages.products_page import Products_page
 from utils.config import Config
@@ -10,10 +12,39 @@ main_page = MainPage()
 
 
 def test_add_product():
-    main_page.open()
+    with allure.step('Open main page'):
+        main_page.open()
 
-    main_page.authorization(login_shop, password_shop)
+    with allure.step('Authorization success'):
+        main_page.authorization(login_shop, password_shop)
 
-    from selene import browser
-    from selene import have
-    browser.element('#add-to-cart-sauce-labs-backpack').click()
+    with allure.step('Add product for basket'):
+        products_page.add_product_for_basket()
+
+    with allure.step('Open basket'):
+        products_page.open_backet()
+
+    with allure.step('Check basket after add product'):
+        products_page.check_success_add_product('Sauce Labs Backpack',
+                                                '29.99')
+
+
+def test_remove_product():
+    with allure.step('Authorization success'):
+        main_page.open()
+
+    with allure.step('Authorization success'):
+        main_page.authorization(login_shop, password_shop)
+
+    with allure.step('Add product for basket'):
+        products_page.add_product_for_basket()
+
+    with allure.step('Open basket'):
+        products_page.open_backet()
+
+    with allure.step('Remove product for basket'):
+        products_page.remove_product_for_basket()
+
+    with allure.step('Check basket after remove product'):
+        products_page.check_after_remove_product()
+
